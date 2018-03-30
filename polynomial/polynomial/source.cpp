6713f574b46d31c738fc1d1022c942c99057df21
Vladislav Tochanenko <itochanenko@gmail.com>
2018-03-30 22:01:21 +0300

    Fourth commmit

--- polynomial/polynomial/source.cpp
+++ polynomial/polynomial/source.cpp
@@ -2,15 +2,5 @@
 
 int main() {
 	// Insert your code here
-	// DELETE -->
-	Polynomial first;
-
-	first.add_monomial(0, 1.5);
-
-	for (int i = 1; i < 5; i++)
-		first.add_monomial(i, 0.8 + i * 1.2);
-
-	first.print_polynomial();
-	////////////////////////////////////////// <--
 	PAUSE;
 }
